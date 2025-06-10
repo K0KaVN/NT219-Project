e712c6b9b810da3eba7a0000dabf87584d18a02c
K0KaVN <144688216+K0KaVN@users.noreply.github.com>
2025-06-10 19:59:31 +0700

    Update PaymentPage.jsx

--- frontend/src/pages/PaymentPage.jsx
+++ frontend/src/pages/PaymentPage.jsx
@@ -5,23 +5,16 @@ import Header from '../components/Layout/Header'
 import Payment from "../components/Payment/Payment.jsx";
 
 const PaymentPage = () => {
-    console.log('PaymentPage is rendering...');
-    
     return (
         <div className='w-full min-h-screen bg-[#f6f9fc]'>
             <Header />
             <br />
             <br />
             <CheckoutSteps active={2} />
-            <div style={{ padding: '20px' }}>
-                <h1>Payment Page Debug</h1>
-                <p>PaymentPage with Header and CheckoutSteps is loading successfully</p>
-            </div>
-            {/* Temporarily comment out other components
             <Payment />
             <br />
             <br />
-            <Footer /> */}
+            <Footer />
         </div>
     )
 }
